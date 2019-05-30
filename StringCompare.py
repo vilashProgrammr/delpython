451c495fd8a72c9a12ca7142ab2b2eebfa4d3401
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-05-30 23:42:17 +0530

    Create StringCompare.py

---
+++ StringCompare.py
@@ -0,0 +1,14 @@
+def main():
+    str1 = input("Enter string1:")
+    str2 = input("Enter string2:")
+   
+    compareStrings(str1, str2)
+    
+def compareStrings(s1, s2):
+    #Uncomment the below lines
+    
+    #if s1==s2:
+    #   print("strings are equal")
+    #else:
+    #   print("strings are not equal")
+    
